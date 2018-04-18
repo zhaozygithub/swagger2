0f21a30b362b08beaa9be12f65781de848f3889d
邵思迁 <shaosq@asiainfo.com>
2018-04-18 12:56:48 +0800

    本地提交swagger

---
+++ src/main/java/com/swagger/saytime/bean/JsonResult.java
@@ -0,0 +1,29 @@
+package com.swagger.saytime.bean;
+
+public class JsonResult {
+
+	private String status = null;
+
+	private Object result = null;
+
+    
+    public String getStatus() {
+        return status;
+    }
+
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+    public Object getResult() {
+        return result;
+    }
+
+    
+    public void setResult(Object result) {
+        this.result = result;
+    }
+
+}
